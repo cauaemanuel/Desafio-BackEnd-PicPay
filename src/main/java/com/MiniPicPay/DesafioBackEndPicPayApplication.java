0c44388dca4d4bad537878aea4b65e46edf86171
caua <emanullcaua8@gmail.com>
2024-07-20 21:00:00 -0300

    first commit

---
+++ src/main/java/com/MiniPicPay/DesafioBackEndPicPayApplication.java
@@ -0,0 +1,13 @@
+package com.MiniPicPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackEndPicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackEndPicPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MiniPicPay/DesafioBackEndPicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MiniPicPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndPicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
